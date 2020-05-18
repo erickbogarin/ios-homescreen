10b947a8854c76a5377c9bcc738d7111b8e9b672
Erick Bogarin <erick.bogarin@gmail.com>
2020-05-17 22:06:36 -0300

    feat: update app packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => {
-  return <div className="App">hi</div>;
-};
+  return <div className="App">hi</div>
+}
 
-export default App;
+export default App
