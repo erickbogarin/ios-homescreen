982d87dbec0abaff46ff47ec174c159e8958504c
Erick Bogarin <erick.bogarin@gmail.com>
2020-06-01 23:05:36 -0300

    feat: change CRA to nextjs

--- src/model/App.tsx
+++
@@ -1,8 +0,0 @@
-export interface App {
-  label: string
-  image: string
-}
-
-export interface AppRow {
-  [key: string]: App[]
-}
--- src/utils/prop.ts
+++
@@ -1,5 +0,0 @@
-function prop<T, K extends keyof T>(obj: T, key: K) {
-  return obj[key]
-}
-
-export default prop
