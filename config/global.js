f759d4ba3c182838da9c5901400b22ed59d9eb88
Erick Bogarin <erick.bogarin@gmail.com>
2020-06-01 23:48:16 -0300

    feat: emotion setup

---
+++ config/global.js
@@ -0,0 +1,62 @@
+import { keyframes, css, Global } from '@emotion/core'
+
+export const globalStyles = (
+  <Global
+    styles={css`
+      html {
+        box-sizing: border-box;
+      }
+      *,
+      *:before,
+      *:after {
+        box-sizing: inherit;
+      }
+      body {
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, system-ui, BlinkMacSystemFont;
+        background: #171617;
+        @media (min-width: 480px) {
+          max-width: 300px;
+          margin: 0 auto;
+        }
+      }
+    `}
+  />
+)
+
+export const shake = keyframes`
+    0% {
+      -webkit-transform: translate(1px, 0px) rotate(0deg);
+    }
+    10% {
+      -webkit-transform: translate(0px, 0px) rotate(-1deg);
+    }
+    20% {
+      -webkit-transform: translate(-1px, 0px) rotate(1deg);
+    }
+    30% {
+      -webkit-transform: translate(0px, 0px) rotate(0deg);
+    }
+    40% {
+      -webkit-transform: translate(1px, -0px) rotate(1deg);
+    }
+    50% {
+      -webkit-transform: translate(0px, 0px) rotate(-1deg);
+    }
+    60% {
+      -webkit-transform: translate(-1px, 0px) rotate(0deg);
+    }
+    70% {
+      -webkit-transform: translate(0px, 0px) rotate(-1deg);
+    }
+    80% {
+      -webkit-transform: translate(1px, 0px) rotate(1deg);
+    }
+    90% {
+      -webkit-transform: translate(0px, 0px) rotate(0deg);
+    }
+    100% {
+      -webkit-transform: translate(-1px, -0px) rotate(-1deg);
+    }
+`
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
