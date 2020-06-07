a0120cd83e661b6854be72fd1e2c348e6df529f4
Erick Bogarin <erick.bogarin@gmail.com>
2020-06-07 13:54:56 -0300

    fix: pwa support

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 const withPWA = require('next-pwa')
 const withImages = require('next-images')
 
-const pwa = (module.exports = withPWA({
-  devIndicators: {
-    autoPrerender: false,
-  },
-  pwa: {
-    dest: 'public',
-  },
-}))
-
-const images = withImages()
-
-module.exports = {
-  ...pwa,
-  ...images,
-}
+module.exports = withImages(
+  withPWA({
+    devIndicators: {
+      autoPrerender: false,
+    },
+    pwa: {
+      dest: 'public',
+    },
+  }),
+)
